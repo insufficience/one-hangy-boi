c94679d3bc6e0db99d17ed1cae66f4c485dca99f
yechan <yechankang6@gmail.com>
2019-05-23 12:10:51 -0700

    idk what changed

--- DesignTeamExpectations.java
+++ DesignTeamExpectations.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public interface DesignTeamExpectations {
 	
 	boolean didYouDoIt(); // returns true if the game is over
-	boolean chooseMode(); // starts up the game and returns true if they choose two player mode
+	boolean chooseMode(); // starts up the game and returns true if they choose two player modeu
 	String getGuess(); // gets the guesses
 }
+                            
