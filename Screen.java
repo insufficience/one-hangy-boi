8af6faf0e256145c672ccec4bfaa2e4d46f513af
ElGranTortuga <50886045+ElGranTortuga@users.noreply.github.com>
2019-05-24 12:22:41 -0700

    screen update

--- Screen.java
+++ Screen.java
@@ -8,10 +8,26 @@ public class Screen implements DesignTeamExpectations, InputKeyControl,InputCont
 	private String a;
 	private boolean askingForGuess = false;
 	private String b;
+	private Rectangle one;
+	private Text single;
+	private Rectangle two;
+	private Text dwouble;
+	private WordDisplay word;
+	private boolean bob;
 	
 	public Screen() {
 		KeyController kC = new KeyController(Canvas.getInstance(), new Screen());
 		MouseController mC = new MouseController(Canvas.getInstance(), new Screen());
+		one = new Rectangle(20, 50, 100,50);
+		two = new Rectangle(140, 50, 100,50);
+		one.setColor(Color.BLUE);
+		two.setColor(Color.BLUE);
+		one.fill();
+		two.fill();
+		single = new Text(30,60,"Single Player");
+		dwouble = new Text(150,60,"Double Player");
+		single.draw();
+		dwouble.draw();
 	}
 	
 	public void keyPress(String es) {
@@ -21,38 +37,23 @@ public class Screen implements DesignTeamExpectations, InputKeyControl,InputCont
 			askingForGuess = false;
 		}
 	}
-	
 	public boolean didYouDoIt() {
 		
 	}
-	
 	public boolean chooseMode() {
-		Rectangle one = new Rectangle(20, 50, 100,50);
-		Rectangle two = new Rectangle(140, 50, 100,50);
-		one.setColor(Color.BLUE);
-		two.setColor(Color.BLUE);
-		one.fill();
-		two.fill();
-		Text single = new Text(30,60,"Single Player");
-		Text dwouble = new Text(150,60,"Double Player");
-		single.draw();
-		dwouble.draw();
-		int count =1000;
-		while(count>0) {
-			count--;
-			Canvas.pause(100);
-			if(ex >= one.getX() && ex <= one.getX()+one.getLength()&&why >= one.getY() && why <= one.getY()+one.getHeight()){
-				return false;
-			}
-			if(ex >= two.getX() && ex <= two.getX()+two.getLength()&&why >= two.getY() && why <= two.getY()+two.getHeight()){
-				return true;
-			}
+		while(){
+			
 		}
-		return false;
+		return bob;
 	}
-	public void onMouseClick(double x, double y){
-		ex=x;
-		why=y;
+	public void onMouseClick(double ex, double why){
+		if(ex >= one.getX() && ex <= one.getX()+one.getLength()&&why >= one.getY() && why <= one.getY()+one.getHeight()){
+			bob= false;
+		}
+		if(ex >= two.getX() && ex <= two.getX()+two.getLength()&&why >= two.getY() && why <= two.getY()+two.getHeight()){
+			bob= true;
+		}
+		
 	}
 	public String getGuess() {
 		askingForGuess = true;
