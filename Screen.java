5e09488b4478da6391930f41a28ea1627d0924a3
ElGranTortuga <50886045+ElGranTortuga@users.noreply.github.com>
2019-05-24 11:53:32 -0700

    gggg

--- Screen.java
+++ Screen.java
@@ -1,18 +1,25 @@
 import pkg.*;
 import java.util.ArrayList;
 
-public class Screen implements DesignTeamExpectations, InputKeyControl {
+public class Screen implements DesignTeamExpectations, InputKeyControl,InputControl {
 	
-	private double ex;
-	private double why;
+	private double ex = 0;
+	private double why = 0;
+	private String a;
+	private boolean askingForGuess = false;
+	private String b;
 	
 	public Screen() {
-		KeyController kC = new KeyController(Canvas.getInstance(), new starter());
-		MouseController mC = new MouseController(Canvas.getInstance(), new starter());
+		KeyController kC = new KeyController(Canvas.getInstance(), new Screen());
+		MouseController mC = new MouseController(Canvas.getInstance(), new Screen());
 	}
 	
 	public void keyPress(String es) {
-		
+		if(askingForGuess) {
+			b = a;
+			a = es;
+			askingForGuess = false;
+		}
 	}
 	
 	public boolean didYouDoIt() {
@@ -34,16 +41,25 @@ public class Screen implements DesignTeamExpectations, InputKeyControl {
 		while(count>0) {
 			count--;
 			Canvas.pause(100);
-			if(ex >= one.getX() && ex <= one.getX()) {
-				
+			if(ex >= one.getX() && ex <= one.getX()+one.getLength()&&why >= one.getY() && why <= one.getY()+one.getHeight()){
+				return false;
+			}
+			if(ex >= two.getX() && ex <= two.getX()+two.getLength()&&why >= two.getY() && why <= two.getY()+two.getHeight()){
+				return true;
 			}
 		}
+		return false;
 	}
 	public void onMouseClick(double x, double y){
 		ex=x;
 		why=y;
 	}
 	public String getGuess() {
-		
+		askingForGuess = true;
+		while(a.equals(b)) {
+			
+		}
+		guess(a);
+		return a;
 	}
 }
